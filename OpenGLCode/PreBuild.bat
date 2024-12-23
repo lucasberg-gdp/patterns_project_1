15ecf22760fd4aa96d575ead6a601d2b24ed0a45
lucasberg29 <lucasberg29@gmail.com>
2024-12-22 20:23:32 -0500

    fixed build structure

--- OpenGLCode/PreBuild.bat
+++ OpenGLCode/PreBuild.bat
@@ -1,2 +1,13 @@
 @echo off
-set PATH=%PATH%;%1
+echo "Starting PreBuild.bat" > "C:\Users\Lucas\OneDrive\Desktop\test\log.txt"
+echo "SOLUTION_DIR: %1" >> "C:\Users\Lucas\OneDrive\Desktop\test\log.txt"
+echo "CONFIGURATION: %2" >> "C:\Users\Lucas\OneDrive\Desktop\test\log.txt"
+
+set SOLUTION_DIR=%1
+set CONFIGURATION=%2
+set OUT_DIR=%3
+
+echo "Copying .dll files to: %OUT_DIR%" >> "C:\Users\Lucas\OneDrive\Desktop\test\log.txt"
+
+xcopy /y /s "%SOLUTION_DIR%dev\fmod\lib\*.dll" "%OUT_DIR%"
+xcopy /y /s "%SOLUTION_DIR%dev\assimp\lib\*.dll" "%OUT_DIR%"
